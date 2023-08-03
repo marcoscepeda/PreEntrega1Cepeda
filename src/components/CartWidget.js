09e82d44c7566d9f7943c16ed42df25e18cedff6
marcoscepeda <marcoshcepeda@gmail.com>
2023-08-02 23:47:43 -0300

    navbar casi terminado

---
+++ src/components/CartWidget.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function CartWidget() {
+  return (
+    <>
+        <i><img src="" alt="" /></i>
+        <p>3</p>
+
+    </>
+    
+  )
+}
+
+export default CartWidget
---
+++ src/components/NavBar.js
@@ -0,0 +1,18 @@
+import CartWidget from "./CartWidget";
+
+function NavBar (){
+
+    return (
+        <header id="header" className=" header flex  navbar navbar-expand-lg bg-light">
+            <h1>Elegancia</h1>
+            <nav className ="navBar  ">
+                <a href="#" className =" mx-4 ">Interior</a>
+                <a href="#" className =" mx-4 ">Exterior</a>
+                <a href="#" className ="mx-4 ">Oficina</a>
+            </nav>
+            <CartWidget/>
+        </header>
+    )
+}
+
+export default NavBar;
